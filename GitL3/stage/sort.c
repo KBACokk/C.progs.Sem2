07bdb9bbac3a78ebe501975ccda304e09875ca87
KBACokk <statsenko228s@gmail.com>
2025-03-09 14:46:42 +0700

    Laba Work #3 Git step ( 1 )

---
+++ GitL3/stage/sort.c
@@ -0,0 +1,17 @@
+int InsertingSort(int arr[], int n) {
+    int T = 0, C = 0, M = 0, temp = 0;
+    for (int i = 1; i < n; i++) {
+        M++; temp = arr[i];
+        int j = i - 1;
+        while (j >= 0 && temp < arr[j]) {
+            M++; arr[j + 1] = arr[j];
+            C++;
+            j--;
+        }
+        if (j >= 0) {
+            C++;
+        }
+        M++; arr[j + 1] = temp;
+    }
+    return T = M + C;
+}
