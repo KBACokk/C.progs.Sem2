b20665f173f1acf7cbb71a59f060a8ccd05b32f6
KBACokk <statsenko228s@gmail.com>
2025-03-09 16:01:17 +0700

    Laba Work #3 Git step ( 2 )

--- GitL3/sort.c
+++ GitL3/sort.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int InsertingSort(int arr[], int n) {
     int T = 0, C = 0, M = 0, temp = 0;
     for (int i = 1; i < n; i++) {
@@ -15,3 +17,12 @@ int InsertingSort(int arr[], int n) {
     }
     return T = M + C;
 }
+
+ int main () {
+	int b = 3;
+	int a[] = {4,0,2};
+	InsertingSort(a, 3);
+    for (int i = 0; i < b; i++) {
+        printf ("%d\n", a[i]);
+    }
+ }
